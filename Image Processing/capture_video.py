1a6a0083971902a87c39c5e28230f296d7057673
DJ-hudl <david.jackson@hudl.com>
2023-11-18 14:58:37 -0600

    Update model

--- Image Processing/capture_video.py
+++ Image Processing/capture_video.py
@@ -2,48 +2,48 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
-#uses the EfficientDet model from the TF2 Model Zoo to detect objects in the webcam feed
-
-# Load the pre-trained EfficientDet model
-model = tf.saved_model.load("efficientdet_d0_coco/saved_model")
-
-def detect_objects(image):
-    # Convert the image to a NumPy array
-    image_np = np.array(image)
-
-    # Convert the NumPy array to a tensor
-    input_tensor = tf.convert_to_tensor([image_np])
-
-    # Perform inference
-    detections = model(input_tensor)
-
-    return detections
-
+# Load the pre-trained Faster R-CNN model
+model_path = 'faster_rcnn_inception_resnet_v2_coco/saved_model'
+detect_fn = tf.saved_model.load(model_path)
 
 # Create a VideoCapture object
-cap = cv2.VideoCapture(0)  # 0 represents the default camera (change if using an external webcam)
+cap = cv2.VideoCapture(0)  # Use 0 for default camera, change if using an external webcam
+
+# Create a background subtractor
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Detect objects in the frame
-    detections = detect_objects(frame)
+    # Apply background subtraction
+    fgmask = fgbg.apply(frame)
 
-    # Display the resulting frame with bounding boxes
-    for detection in detections['detection_boxes']:
-        ymin, xmin, ymax, xmax = detection.numpy()[0]
-        xmin = int(xmin * frame.shape[1])
-        xmax = int(xmax * frame.shape[1])
-        ymin = int(ymin * frame.shape[0])
-        ymax = int(ymax * frame.shape[0])
+    # Threshold the foreground mask to get binary image
+    _, thresh = cv2.threshold(fgmask, 25, 255, cv2.THRESH_BINARY)
 
-        # Draw a bounding box around the detected object
-        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+    # Find contours in the binary image
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Draw bounding boxes around moving objects
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Crop the region of interest (ROI) around the detected object
+        roi = frame[y:y + h, x:x + w]
+
+        # Detect if a train is present in the ROI
+        detections = detect_fn(tf.convert_to_tensor([roi]))
+        for detection in detections['detection_classes']:
+            if detection == 44:  # 44 corresponds to the class label for 'train' in COCO dataset
+                # Save the image with a train to a new file
+                cv2.imwrite('train_detected_image.jpg', frame)
+                print('Train detected! Image saved.')
+                break
 
     # Display the resulting frame
-    cv2.imshow('Webcam', frame)
+    cv2.imshow('Object Motion Detection', frame)
 
     # Break the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
